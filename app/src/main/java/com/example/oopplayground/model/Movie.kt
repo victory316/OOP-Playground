558206e0e8c1d9357773a1e684c8cef019381a6e
SH CHoi <csh153@gmail.com>
2023-08-24 17:11:49 +0900

    Kotlin style로 개선

--- app/src/main/java/com/example/oopplayground/model/Movie.kt
+++ app/src/main/java/com/example/oopplayground/model/Movie.kt
@@ -4,20 +4,19 @@ import com.example.oopplayground.model.discounts.policy.DiscountPolicy
 import java.time.Duration
 
 class Movie(
-    title: String,
-    runningTime: Duration,
-    fee: Money,
-    discountPolicy: DiscountPolicy,
+    private val title: String,
+    private val runningTime: Duration,
+    private val fee: Money,
+    private var discountPolicy: DiscountPolicy
 ) {
-
-    private val title: String = title
-    private val runningTime: Duration = runningTime
-    private val fee: Money = fee
-    private val discountPolicy: DiscountPolicy = discountPolicy
 
     fun getFee(): Money = fee
 
     fun calculateMovieFee(screening: Screening): Money {
         return fee.minus(discountPolicy.calculateDiscountAmount(screening))
+    }
+
+    fun changeDiscountPolicy(policy: DiscountPolicy) {
+        discountPolicy = policy
     }
 }
